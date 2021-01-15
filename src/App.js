e6526ea807436b65d63ddf7f5bf85c898f36c16e
Kioshi Okamoto <kjor29@gmail.com>
2021-01-15 15:02:33 -0500

    :bug: FIX: Solucion de problema con variables de entorno

--- src/App.js
+++ src/App.js
@@ -10,15 +10,15 @@ import { Channel } from './components/Channel';
 
 if (!firebase.apps.length) {
 	firebase.initializeApp({
-		apiKey: process.env.API_KEY,
-		authDomain: process.env.AUTH_DOMAIN,
-		projectId: process.env.PROJECT_ID,
-		storageBucket: process.env.STORAGE_BUCKET,
-		messagingSenderId: process.env.MESSAGING_SENDER_ID,
-		appId: process.env.APP_ID,
+		apiKey: process.env.REACT_APP_API_KEY,
+		authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+		projectId: process.env.REACT_APP_PROJECT_ID,
+		storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+		messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+		appId: process.env.REACT_APP_APP_ID,
 	});
 } else {
-	firebase.app(); // if already initialized, use that one
+	firebase.app(); // Si esta inicializado, usa la app
 }
 
 const db = firebase.firestore();
@@ -69,7 +69,9 @@ function App() {
           <Channel user={user} db={db}></Channel>
         </> 
       : 
-        <Button onclick={signInWithGoogle}>Iniciar sesión con Google</Button>}</div>
+        
+        <Button onclick={signInWithGoogle}>Iniciar sesión con Google</Button>}
+    </div>
 	);
 }
 
